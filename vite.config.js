3c80ab62ac31b6f5f0c4b2eb4ef83e398885e7a1
Iftikhar Ali <rikbcs204@gmail.com>
2025-11-17 14:45:07 +0500

    Delete Vite configuration file
    Remove Vite configuration for Laravel plugin

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,1 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            refresh: true,
-        }),
-    ],
-});
